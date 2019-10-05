0336fab8d2a01febb6932b7ffac9a127dcdbed6b
A.Farahani <arashfarahani@hotmail.com>
2019-10-05 10:41:15 +0330

    Websphere Mq has been tested

--- test/java/com/core/mqutil/MqManagerTest.java
+++ test/java/com/core/mqutil/MqManagerTest.java
@@ -1,13 +1,19 @@
 package com.core.mqutil;
 
 import com.core.common.CoreException;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import java.io.IOException;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MqManagerTest {
     private final static String TEST_MESSAGE = "This message is just for test";
+
     private MqManager mqManager;
 
     @Before
@@ -18,20 +24,21 @@ public class MqManagerTest {
         config.setChannel("Test");
         config.setCsid(1208);
         config.setHostName("Test");
-        config.setPassword("Test");
-        config.setPort(1919);
+        config.setPort(14024);
         config.setQueueManager("Test");
         config.setUserId("Test");
         config.setPassword("Test");
-        this.mqManager.open(new MqConfig());
+        this.mqManager.open(config);
     }
 
     @Test
+    @Order(1)
     public void write() throws CoreException {
         this.mqManager.write(TEST_MESSAGE);
     }
 
     @Test
+    @Order(2)
     public void read() throws CoreException {
         this.mqManager.next();
         String message = this.mqManager.read();
@@ -39,11 +46,12 @@ public class MqManagerTest {
     }
 
     @Test
+    @Order(3)
     public void remove() throws CoreException {
         this.mqManager.remove();
     }
 
-    @Before
+    @After
     public void close() throws IOException {
         this.mqManager.close();
     }
